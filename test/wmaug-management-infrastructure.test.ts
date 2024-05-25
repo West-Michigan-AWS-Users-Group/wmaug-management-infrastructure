05d7f60f990db3dcefb2050b1c6b5865e4f32a7c
tnielsen2 <trentnielsen84@gmail.com>
2024-05-25 07:50:55 -0400

    Add permissionset resource

---
+++ test/wmaug-management-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WmaugManagementInfrastructure from '../lib/wmaug-management-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wmaug-management-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WmaugManagementInfrastructure.WmaugManagementInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
